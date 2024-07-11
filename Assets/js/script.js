55ea74252997e0405f9e8928d2be93dc7a011c51
jack <jackrozovsky@gmail.com>
2024-07-10 21:50:07 -0400

    Commit

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,7 +2,8 @@ const API = `a1d90eb1b487faeb0b94aa3e66ed15e0`;
 const searchinput = document.getElementById(`searchinput`);
 const searchButton = document.getElementById(`searchButton`);
 const storedcities = JSON.parse(localStorage.getItem(`cities`)) || [];
-
+const container = document.getElementById(`container`);
+const btncontainer = document.getElementById('buttons');
 
 function getdata(){
     let city = searchinput.value.trim();
@@ -29,7 +30,6 @@ function renderCurrentWeather(city,weather) {
     const humid = weather.list[0].main.humidity;
     const icon = weather.list[0].weather[0].icon;
     const iconURL =`https://openweathermap.org/img/wn/${icon}.png`;
-    const container = document.getElementById(`container`);
     const windh1 = document.createElement(`h1`);
     const humidh1 = document.createElement(`h1`);
     const iconimg = document.createElement(`img`);
@@ -45,5 +45,31 @@ function storesearchHistory(city) {
     storedcities.push(city);
     localStorage.setItem(`cities`,JSON.stringify(storedcities));
 }
+function createHistoryButton() {
+    const thisstoredcity = JSON.parse(localStorage.getItem(`cities`))
+
+    for (let i = 0; i < thisstoredcity.length; i++) {
+        const newbutton =   document.createElement(`button`);
+        newbutton.classList.add(`historybtn`)
+        newbutton.textContent = thisstoredcity;
+        btncontainer.append(newbutton);
+    };
+    
+
+}
+
+function handlesearchhistory(e) {
+    if(!e.target.matches(`.historybtn`)){
+        return; 
+    }
+    const target = e.target;
+    const cityname = target.texetcontent;
+    renderCurrentWeather(cityname);
+}
+
+
+
+createHistoryButton();
+btncontainer.addEventListener(`click`, handlesearchhistory);
 
 searchButton.addEventListener(`click`,getdata);
